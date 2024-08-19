c30037335adbb45e98796d222cfa4d145a172981
divyanshyadav <divyanshyadav20@gmail.com>
2024-08-19 12:40:01 +0530

    :bug: Fixed drawer not closing on route change

--- components/navbar/Toggle.tsx
+++ components/navbar/Toggle.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import {
   Drawer,
+  DrawerClose,
   DrawerContent,
   DrawerDescription,
   DrawerHeader,
@@ -24,22 +25,23 @@ function Toggle() {
         </DrawerTrigger>
         <DrawerPortal>
           <DrawerOverlay className="fixed inset-0 bg-black/40" />
-          <DrawerContent>
-            <DrawerHeader>
+          <DrawerContent className="flex flex-col gap-5 p-5">
+            {/* Need to keep it here like this otherwise there is an error in the console for missing drawer title */}
+            <DrawerHeader className="hidden">
               <DrawerTitle></DrawerTitle>
               <DrawerDescription></DrawerDescription>
             </DrawerHeader>
-            <div className="flex flex-col gap-5 p-5">
-              {FOOTER_LINKS.map(({ name, href }, index) => (
+
+            {FOOTER_LINKS.map(({ name, href }, index) => (
+              <DrawerClose key={index} asChild>
                 <Link
                   className="text-xl transition-colors duration-300 hover:text-indigo-600"
                   href={href}
-                  key={index}
                 >
                   {name}
                 </Link>
-              ))}
-            </div>
+              </DrawerClose>
+            ))}
           </DrawerContent>
         </DrawerPortal>
       </Drawer>
