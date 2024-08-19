488f8aa6e59efef60b62250097a74770b93b9610
divyanshyadav <divyanshyadav20@gmail.com>
2024-08-19 15:56:55 +0530

    :sparkles: Added the cart functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Cart from "@/components/Cart";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "@/components/Providers";
@@ -22,13 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="brand-logo.png" />
-      </head>
       <body className={inter.className}>
         <Providers>
           <Navbar />
           {children}
+          <Cart />
           <Footer />
         </Providers>
       </body>
