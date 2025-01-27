633f33bd78fdf1cc3367170d1379ce86b696b7cb
Divyansh Yadav <divyanshyadav20@gmail.com>
2025-01-27 16:58:26 +0530

    Made home server based

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
-"use client";
-
 import FeaturedProducts from "@/components/FeaturedProducts";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Promo from "@/components/Promo";
 import Reveal from "@/components/Reveal";
-import useFetchProducts from "@/hooks/useFetchProducts";
 import { ProductCategory } from "@/models";
-import { useEffect } from "react";
 
 export default function Home() {
-  const { products, fetchProducts } = useFetchProducts();
-
-  useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
-
-  console.log({ products });
-
   return (
     <div>
       <Hero />
